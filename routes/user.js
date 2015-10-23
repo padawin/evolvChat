293e986ebc478e96d318ddd7f8f38430d7cb48ca
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-23 09:56:48 +0100

    route to decide if a user exists or not

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,24 @@ var express = require('express'),
 	router = express.Router();
 
 module.exports = function (io) {
+	var session;
 	io.on('connection', function(socket) {
 		socket.join(socket.handshake.query.room);
 		console.log(socket.handshake.query.nickname, socket.handshake.query.room);
+		session.user = socket.handshake.query;
+	});
+
+	// When a user load the page, to know if a session exists
+	router.post('/enter', function(req, res) {
+		res.setHeader('content-type', 'application/json');
+
+		if (typeof(session) == 'object' && 'user' in session) {
+			res.json({nickname: session.user.nickname, room: session.user.room});
+		}
+		else {
+			session = req.session;
+			res.json(null);
+		}
 	});
 
 	return router;
