bf3ce6ca5ea116e6da13fa27b8cdae729179568e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-23 16:58:34 +0100

    session was shared with everybody!

--- routes/user.js
+++ routes/user.js
@@ -2,22 +2,18 @@ var express = require('express'),
 	router = express.Router();
 
 module.exports = function (io) {
-	var session;
 	io.on('connection', function(socket) {
 		socket.join(socket.handshake.query.room);
-		console.log(socket.handshake.query.nickname, socket.handshake.query.room);
-		session.user = socket.handshake.query;
 	});
 
 	// When a user load the page, to know if a session exists
 	router.post('/enter', function(req, res) {
 		res.setHeader('content-type', 'application/json');
 
-		if (typeof(session) == 'object' && 'user' in session) {
-			res.json({nickname: session.user.nickname, room: session.user.room});
+		if ('user' in req.session) {
+			res.json({nickname: req.session.user.nickname, room: req.session.user.room});
 		}
 		else {
-			session = req.session;
 			res.json(null);
 		}
 	});
@@ -25,7 +21,7 @@ module.exports = function (io) {
 	// When a user load the page, to know if a session exists
 	router.post('/logout', function(req, res) {
 		res.setHeader('content-type', 'application/json');
-		delete session.user;
+		delete red.session.user;
 		res.json(['OK']);
 	});
 
