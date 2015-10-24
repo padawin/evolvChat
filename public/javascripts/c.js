21c4c90ed5fb73b80fb2aa59eac42e5c3c08223e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-24 13:57:53 +0100

    skeletton of the parse method to detect which template operation to execute

--- public/javascripts/c.js
+++ public/javascripts/c.js
@@ -4,8 +4,25 @@ if (typeof (require) != 'undefined') {
 
 loader.addModule('c', function () {
 	var c = {},
+		_parseMatch,
 		regexTemplate = /\[\[(.+?)]]/g,
 		savedTemplates = {};
+
+	_parseMatch = function (template) {
+		/*
+		 * allowed commands:
+		 * expression
+		 * expression.attribute
+		 * each expression on template
+		 * if expression then template
+		 */
+		var regexExpression = /^[a-zA-Z_$][0-9a-zA-Z_$]*(?:\.[a-zA-Z_$][0-9a-zA-Z_$]*)*$/g,
+			regexEach = /^each\s+[a-zA-Z_$][0-9a-zA-Z_$]*\s+on\s+[a-zA-Z_$\-0-9]+$/,
+			regexIf = /^if\s+[a-zA-Z_$][0-9a-zA-Z_$]*\s+then\s+[a-zA-Z_$\-0-9]+$/;
+		console.log(regexExpression.exec(template));
+		console.log(regexEach.exec(template));
+		console.log(regexIf.exec(template));
+	};
 
 	c.url = function (url, data, container, callback) {
 		if (savedTemplates[url]) {
@@ -23,6 +40,8 @@ loader.addModule('c', function () {
 	c.template = function (template, data, callback) {
 		var match = regexTemplate.exec(template);
 		while (match !== null) {
+			// parse match
+			_parseMatch(match[1]);
 			template = template.replace(match[0], data[match[1]]);
 			match = regexTemplate.exec(template);
 		}
