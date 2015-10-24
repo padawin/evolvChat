62f3ef7f2f515bab6d41b774e205149f90cb92d4
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-24 17:57:19 +0100

    fields captured

--- public/javascripts/c.js
+++ public/javascripts/c.js
@@ -18,9 +18,9 @@ loader.addModule('c', function () {
 		 * each expression on template
 		 * if expression then template
 		 */
-		var regexExpression = /^[a-zA-Z_$][0-9a-zA-Z_$]*(?:\.[a-zA-Z_$][0-9a-zA-Z_$]*)*$/g,
-			regexEach = /^each\s+[a-zA-Z_$][0-9a-zA-Z_$]*\s+on\s+[a-zA-Z_$\-0-9]+$/,
-			regexIf = /^if\s+[a-zA-Z_$][0-9a-zA-Z_$]*\s+then\s+[a-zA-Z_$\-0-9]+$/;
+		var regexExpression = /^([a-zA-Z_$][0-9a-zA-Z_$]*)(?:\.([a-zA-Z_$][0-9a-zA-Z_$]*))*$/g,
+			regexEach = /^each\s+([a-zA-Z_$][0-9a-zA-Z_$]*)\s+on\s+([a-zA-Z_$\-0-9]+)$/,
+			regexIf = /^if\s+([a-zA-Z_$][0-9a-zA-Z_$]*)\s+then\s+([a-zA-Z_$\-0-9]+)$/;
 		console.log(regexExpression.exec(template));
 		console.log(regexEach.exec(template));
 		console.log(regexIf.exec(template));
