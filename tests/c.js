b27045809721a7a94eff173a2dd2a07e55c4698f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-10-26 13:01:20 +0000

    skeletton of the tests for c

---
+++ tests/c.js
@@ -0,0 +1,55 @@
+if (typeof (require) != 'undefined') {
+	var loader = require('../public/javascripts/loader.js').loader;
+}
+
+loader.executeModule('cTests', 'c', 'Tests', function (c, Tests) {
+	Tests.addSuite('c', [
+		/**
+		 * Test if the methods exist
+		 */
+		function() {
+			Tests.isA(c.init, 'function');
+			Tests.isA(c.compile, 'function');
+		},
+
+		/**
+		 * Test to compile a template from an url
+		 */
+
+		/**
+		 * Test to compile a template from a html
+		 */
+
+		/**
+		 * Test to compile a static template
+		 */
+
+		/**
+		 * Test to compile a template with a simple expression
+		 */
+
+		/**
+		 * Test to compile a template with an expression's attribute
+		 */
+
+		/**
+		 * Test to compile a template with two expressions on the same line
+		 */
+
+		/**
+		 * Test to compile a template with two expressions on different lines
+		 */
+
+		/**
+		 * Test to compile a template with a each call
+		 */
+
+		/**
+		 * Test to compile a template with a if call
+		 */
+
+		/**
+		 * Test to compile a template with an unprovided data
+		 */
+	]);
+});
